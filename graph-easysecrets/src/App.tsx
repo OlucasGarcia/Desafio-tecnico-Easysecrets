896766fb1e98b2608033f40b5576e0f97edfafef
Lucas Gabriel Garcia <130869029+OlucasGarcia@users.noreply.github.com>
2025-06-13 18:34:29 -0300

    Revert "Task 02 "

--- graph-easysecrets/src/App.tsx
+++ graph-easysecrets/src/App.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import './App.css';
 
-//lib de rotas
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-//components de páginas
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
-
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route 
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-        <Route 
-          path="*"
-          element={<NotFound />}
-        />
-
-      </Routes>
-    </Router>
+    <div className="App">
+    </div>
   );
 }
 
