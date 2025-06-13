b77ce2ad78b70602826fa207a8dfa37afb4a209a
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-06-12 22:21:30 -0300

    chore - estruturação das rotas da aplicação

--- graph-easysecrets/src/App.tsx
+++ graph-easysecrets/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
 
+//lib de rotas
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//components de páginas
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route 
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+        <Route 
+          path="*"
+          element={<NotFound />}
+        />
+
+      </Routes>
+    </Router>
   );
 }
 
