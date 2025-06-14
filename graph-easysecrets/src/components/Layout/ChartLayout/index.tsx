cea8b6e7423c5a5c8267a4fc1d9b94562041ac26
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2025-06-14 20:23:21 -0300

    TASK-04: style - implementação de responsividade e adição de animação visual ao componente de Layout ChartLayout

--- graph-easysecrets/src/components/Layout/ChartLayout/index.tsx
+++ graph-easysecrets/src/components/Layout/ChartLayout/index.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import styles from './ChartLayout.module.css';
 
-//react icons imports
 import { FaChartLine } from "react-icons/fa";
 import { FaChartBar } from "react-icons/fa";
 import { FaChartArea } from "react-icons/fa";
 
-//chart components imports
 import ChartLineComponent from "../../Charts/ChartLine";
 import ChartBarComponent from "../../Charts/ChartBar";
 import ChartAreaComponent from "../../Charts/ChartArea";
-import exportarPDF from "../../../utils/exportPDF";
 import Button from "../../Button";
+
+import exportarPDF from "../../../utils/exportPDF";
+
+import { motion } from "framer-motion";
 
 function ChartLayout() {
 
@@ -19,43 +20,40 @@ function ChartLayout() {
 
     return (
         <div className={styles.layout}>
-            <p>
-                Escolha o tipo de gráfico:
-            </p>
-            <div className={styles.btn}>
-                <button 
-                onClick={() => setTipo("linha")}
-                className={`${tipo === "linha" ? styles.active : ''}`}
-                >
-                    <FaChartLine
-                        size={34}
-                    />
-                </button>
-                <button 
-                onClick={() => setTipo("barra")}
-                className={`${tipo === "barra" ? styles.active : ''}`}
-                
-                >
-                    <FaChartBar
-                        size={34}
-                    />
-                </button>
-                <button 
-                onClick={() => setTipo("area")}
-                className={`${tipo === "area" ? styles.active : ''}`}
-                
-                >
-                    <FaChartArea
-                        size={34}
-                    />
-                </button>
-            </div>
-            <div className={styles.chartsDiv} id="grafico">
-                {tipo === "linha" && <><p>GRÁFICO DE LINHAS</p><ChartLineComponent /></>}
-                {tipo === "barra" && <><p>GRÁFICO DE BARRAS</p><ChartBarComponent /></>}
-                {tipo === "area" && <><p>GRÁFICO DE ÁREAS</p><ChartAreaComponent /></>}
-            </div>
-            <Button label="Exportar como PDF" onClick={() => exportarPDF('grafico')}/>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+                style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}
+            >
+                <p>
+                    Escolha o tipo de gráfico:
+                </p>
+                <div className={styles.btn}>
+                    <button
+                        onClick={() => setTipo("linha")}
+                        className={`${tipo === "linha" ? styles.active : styles.desactive}`} >
+                        <FaChartLine size={34} />
+                    </button>
+                    <button
+                        onClick={() => setTipo("barra")}
+                        className={`${tipo === "barra" ? styles.active : styles.desactive}`} >
+                        <FaChartBar size={34} />
+                    </button>
+                    <button
+                        onClick={() => setTipo("area")}
+                        className={`${tipo === "area" ? styles.active : styles.desactive}`} >
+                        <FaChartArea size={34} />
+                    </button>
+                </div>
+
+                <div className={styles.chartsDiv} id="grafico">
+                    {tipo === "linha" && <><p>GRÁFICO DE LINHAS</p><ChartLineComponent /></>}
+                    {tipo === "barra" && <><p>GRÁFICO DE BARRAS</p><ChartBarComponent /></>}
+                    {tipo === "area" && <><p>GRÁFICO DE ÁREAS</p><ChartAreaComponent /></>}
+                </div>
+                <Button label="Exportar como PDF" onClick={() => exportarPDF('grafico')} />
+            </motion.div>
         </div>
     )
 }
